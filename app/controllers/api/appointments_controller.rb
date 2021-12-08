3eb49a4e19a18dccafb61f1ee693da526af7db7f
Sky Lin <linsky519@gmail.com>
2021-12-08 15:42:47 -0700

    routes and controllers done

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,2 +1,43 @@
 class Api::AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :destroy, :update]
+
+  def index
+    render json: Appointment.all_and_then_some
+  end
+
+  def show
+    render json: @appointment
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if (@appointment.save)
+      render json: @appointment
+    else
+      render json: { error: @appointment.errors }, status: 422
+    end
+  end
+
+  def update
+    if (@appointment.update(appointment_params_update))
+      render json: @appointment
+    else
+      render json: { error: @appointment.errors }, status: 422
+    end
+  end
+
+  private
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :description, :user_id, :doctor_id)
+  end
+
+  def appointment_params_update
+    params.require(:appointment).permit(:date, :description)
+  end
+
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+  def self.all_and_then_some
+    appointments = Appointment.all
+    appointments.map do |appointment|
+      {id: appointment.id, date: appointment.date, description: appointment.description, user: appointment.user, doctor: appointment.doctor}
+    end
+  end
 end
