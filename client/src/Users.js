c76f8267ffceeed107b46c56809859b019802748
Madeline Armstrong <armstrong.madeline@gmail.com>
2021-12-08 17:26:27 -0700

    started building add user functionality

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button, Card } from "semantic-ui-react";
+import UserForm from "./UserForm";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
-  
+  const [new_first_name, setNew_first_name] = useState("");
+  const [new_last_name, setNew_last_name] = useState("");
+  const [new_age, setNew_age] = useState("");
+  const [new_gender, setNew_gender] = useState("");
+
   useEffect(() => {
     getData();
   }, []);
@@ -13,6 +18,11 @@ const Users = () => {
     let usersRes = await axios.get("/api/users");
     setUsers(usersRes.data);
     console.log(usersRes.data);
+  }
+
+  const addUser = async () => {
+    const newUser = {first_name: new_first_name, last_name: new_last_name, gender: new_gender, age: new_age}
+    let res = await axios.post("/api/users", newUser);
   }
 
   const renderUsers = () => {
@@ -46,6 +56,7 @@ const Users = () => {
 
   return (
     <div> 
+      
       <h1> List of Users </h1>
       <Card.Group>{renderUsers()}</Card.Group>
     </div>
